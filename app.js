abd1bf8c4c417d6287799d26125d16b10f392272
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-02-02 01:28:06 +0500

    added a js chapter 05 assignment

---
+++ app.js
@@ -0,0 +1,167 @@
+// Chapter no 05 ( math expression )
+
+//Question no 01
+
+// var num1 = 20; 
+// var prep = " and "
+// var num2 = 50;
+// var prepo = " is "
+// var total =  num1 + num2;
+// document.write( "Sum of " + num1  +prep + num2 +prepo + total  );
+
+//Question no 02
+
+// **Subtraction**
+
+// var total =  num1 - num2;
+// document.write( "</br>Subtraction of " + num1  +prep + num2 +prepo + total  );
+
+// **Multiplication**
+
+// var total =  num1 * num2;
+// document.write( "</br>Product of " + num1  +prep + num2 +prepo + total  );
+
+// **Division**
+
+// var total =  num1 / num2;
+// document.write( "</br>Division of " + num1  +prep + num2 +prepo + total  );
+
+// **Modulus**
+
+// var total =  num1 % num2;
+// document.write( "</br>Modulus of " + num1  +prep + num2 +prepo + total  );
+
+
+//Question no 03
+
+// var value;
+// document.write("Value of variable after declaration is "+value);
+// value = 8;
+// document.write("</br>Initial value is "+ value);
+// var valueInc = ++value;
+// document.write("</br>Value after Increment is "+valueInc);
+// var sum = valueInc + 7 ;
+// document.write("</br>Value after addition is "+sum);
+// var valueDec = --sum;
+// document.write("</br>Value after Decrement is " + valueDec);
+// var div = valueDec % 3 ;
+// document.write('</br>Output: "The remainder is : " '+div);
+
+
+// Question no 04
+
+// var costOfTicket = 600;
+// var totalCost = costOfTicket * 5;
+// document.write("Total Cost to buy 5 movie ticket is " +totalCost );
+// document.write("PKR");
+
+// Question no 05
+
+
+
+
+
+// Question no 06
+
+// var C = 25 ;
+// var fToC = (C*9/5)+32 ;
+// document.write( C+"C° is "+fToC )
+// document.write(" F°</br>")
+
+// var F = 70 ;
+// var cToF = (F-32)*5/9 ;
+// document.write( F+"F° is "+cToF )
+// document.write(" C°")
+
+// Question no 07
+
+// document.write("<h1>Shopping Cart</h1>")
+// var priceItem1 = 650;
+// document.write("Price Item of 1 is " + priceItem1);
+// var quantityItem1 = 5;
+// document.write("</br>Quantity of Item 1 is " + quantityItem1 );
+// var priceItem2 = 100;
+// document.write("</br>Price Item of 2 is " + priceItem2);
+// var quantityItem2 = 7;
+// document.write("</br>Quantity of Item 2 is " + quantityItem2);
+// var shippingCharges = 100;
+// document.write("</br>Shipping Charges is " + shippingCharges);
+
+
+// var calculation = (priceItem1*quantityItem1)+(priceItem2*quantityItem2);
+// document.write("</br></br></br>Total Cost of your Order "+ calculation)
+// document.write(" /-Rs")
+
+
+
+//Question no 08
+
+// document.write("<h1>MarksSheets</h1>")
+//  var marksObtained = 804 ;
+//  var marksTotal = 980 ;
+// var percentage = (marksObtained/marksTotal)*100;
+
+// document.write("Total Marks is: "+marksTotal);
+// document.write("</br>Marks Obtained is: "+marksObtained);
+// document.write("</br>Percentage is: "+percentage);
+// document.write("%");
+
+
+// Question no 09
+// document.write("<h1>CURRENCY IN PKR</h1>")
+// var noOfDollors = 10;
+// var valueOf1Dollor = 104.80; 
+// var noOfRiyals = 25;
+// var valueOf1Riyal = 28;
+// var conversion = (noOfDollors*valueOf1Dollor)+(noOfRiyals*valueOf1Riyal);
+// document.write("Total Currency in PKR:" +conversion);
+
+
+// Question no 10
+
+// var number;
+// number = 2;
+// var solution = (number + 5) * 10 / 2 ;
+//  console.log(solution);
+
+//Question no 11
+
+// document.write("<h1>AGE CALCULATOR</h1>");
+
+// var currentYear = 2023;
+// var birthYear = 2004;
+// var calc = currentYear-birthYear;
+// document.write("Current Year:"+currentYear)
+// document.write("</br>Birth Year:"+birthYear)
+// document.write("</br>Your Age is:"+calc)
+
+
+// Question no 12
+document.write("<h1>The Geometrizer</h1>")
+var r = 20;
+document.write("Radius of a circle is: "+r)
+var 𝝅 = 3.142;
+document.write("</br>Circumference of circle is :"+ 2*𝝅*r)
+document.write("</br>Area of circle is:"+𝝅*r*r);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
